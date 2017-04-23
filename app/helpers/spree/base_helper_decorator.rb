5e3cb64bb1a36f170a9b7068a170a93c30f39f03
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-22 22:42:30 -0400

    gray

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -25,5 +25,21 @@ module Spree
         basic_body_class
       end
     end
+
+    def link_to_cart(text = nil)
+      text = text ? h(text) : Spree.t(:cart)
+      css_class = nil
+
+      if simple_current_order.nil? || simple_current_order.item_count.zero?
+        text = "#{text}: (#{Spree.t(:empty)})"
+        css_class = 'empty'
+      else
+        text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
+        css_class = 'full'
+      end
+      link_to text.html_safe,
+              spree.cart_path,
+              class: "cart-info #{css_class} mdl-navigation__link mdl-typography--text-uppercase"
+    end
   end
 end
