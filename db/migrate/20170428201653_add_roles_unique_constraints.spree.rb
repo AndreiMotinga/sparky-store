e368516eb1b93f6c3bdc797af0b927828a5066f3
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-28 16:54:36 -0400

    adds migration

--- db/migrate/20170428201653_add_roles_unique_constraints.spree.rb
+++ db/migrate/20170428201653_add_roles_unique_constraints.spree.rb
@@ -1,6 +1,7 @@
 # This migration comes from spree (originally 20170422134804)
 class AddRolesUniqueConstraints < ActiveRecord::Migration[5.0]
   def change
+    return if ENV["DATABASE"] == "sparky"
     add_index :spree_roles, :name, unique: true
     add_index :spree_roles_users, [:user_id, :role_id], unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427152528) do
+ActiveRecord::Schema.define(version: 20170428201653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -706,6 +706,7 @@ ActiveRecord::Schema.define(version: 20170427152528) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["name"], name: "index_spree_roles_on_name", unique: true, using: :btree
   end
 
   create_table "spree_roles_users", force: :cascade do |t|
@@ -714,6 +715,7 @@ ActiveRecord::Schema.define(version: 20170427152528) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["role_id"], name: "index_spree_roles_users_on_role_id", using: :btree
+    t.index ["user_id", "role_id"], name: "index_spree_roles_users_on_user_id_and_role_id", unique: true, using: :btree
     t.index ["user_id"], name: "index_spree_roles_users_on_user_id", using: :btree
   end
 
