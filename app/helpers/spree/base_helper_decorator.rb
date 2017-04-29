5a9b3ff24a5798ba5ed35011df4a637d9b1f2d71
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-29 18:19:55 -0400

    banner

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -4,14 +4,14 @@ module Spree
       link_to image_tag(image_path, class: classes), spree.root_path
     end
 
-    def taxons_tree(root_taxon, current_taxon, max_level = 1)
-      return "" if max_level < 1 || root_taxon.children.empty?
-      taxons = root_taxon.children.map do |taxon|
-        link_to(taxon.name, seo_url(taxon), class: "mdl-navigation__link") +
-          taxons_tree(taxon, current_taxon, max_level - 1)
-      end
-      safe_join(taxons, "\n")
-    end
+    # def taxons_tree(root_taxon, current_taxon, max_level = 1)
+    #   return "" if max_level < 1 || root_taxon.children.empty?
+    #   taxons = root_taxon.children.map do |taxon|
+    #     link_to(taxon.name, seo_url(taxon), class: "mdl-navigation__link") +
+    #       taxons_tree(taxon, current_taxon, max_level - 1)
+    #   end
+    #   safe_join(taxons, "\n")
+    # end
 
     # NOTE: solidus frontend takes presedence over flutie body_class
     def body_class(options = {})
