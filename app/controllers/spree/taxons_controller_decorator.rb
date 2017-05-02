0b3592c960c4ceda5cb92f9f41a369e3fbe368b8
Andrei Motinga <andrew.motinga@gmail.com>
2017-05-02 13:53:55 -0400

    fixes talons search

---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,15 @@
+# fixes https://github.com/solidusio-contrib/solidus_globalize/issues/18
+# spree core should be updated to allow for more flexible search
+
+module Spree
+  TaxonsController.class_eval do
+    def show
+      @taxon = Taxon.find_by!(permalink: params[:id])
+      return unless @taxon
+
+      @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+      @products = @searcher.retrieve_products
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
+  end
+end
