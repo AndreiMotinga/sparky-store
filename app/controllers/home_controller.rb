2db2f56224116710a6bfef96ad0679d97179c221
Andrei Motinga <andrew.motinga@gmail.com>
2017-05-09 11:21:56 -0400

    cert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < Spree::BaseController
   def about; end
   def contacts; end
+  def key
+    render text: "GwtA-qYQBjcyW1yS7oCX3AaAFIrbve2ISUGM8UoOcgM.b1PiULHHoNe11AhEMh8b1ItLr7bUVus_Xl3SSMee7O4"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "compare", to: "compare#index"
   get "home/about", as: :about
   get "home/contacts", as: :contacts
+  get ".well-known/acme-challenge/CwQCcz0QWevcEq_EBM_RIvFvTuPN6TKd1XrwzxkkcNg", to: "home#key"
   mount Spree::Core::Engine, at: "/"
 end
